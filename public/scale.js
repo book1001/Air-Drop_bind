091f94ee18cf0b6095fd5f9a225f8461293a7e5d
Halim <book1001.db@gmail.com>
2025-11-09 16:49:48 -0500

    mobile test

--- public/scale.js
+++ public/scale.js
@@ -9,7 +9,12 @@ function scaleInputs() {
   const baseDeviceW = 375;
   const baseDeviceH = 667;
 
-  const scale = Math.min(wrapperW / baseDeviceW, wrapperH / baseDeviceH);
+  let scale = Math.min(wrapperW / baseDeviceW, wrapperH / baseDeviceH);
+
+  // 기준 기기보다 작을 경우 scale이 줄어드는 것을 방지
+  if (wrapperW <= baseDeviceW && wrapperH <= baseDeviceH) {
+    scale = 1;
+  }
 
   pages.style.transform = `scale(${scale})`;
 }
